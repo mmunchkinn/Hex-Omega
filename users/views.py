9250a6b7ff183836f2b6e610a4894b5c00e2d08e
Unknown <defstryker@hotmail.com>
2017-03-03 16:20:31 +0800

    altered view template

--- users/views.py
+++ users/views.py
@@ -13,32 +13,42 @@ def search_form(request):
 
 
 def search(request):
-    if 'q' in request.GET and request.GET['q'] is not '':
+    errors = []
+    if 'q' in request.GET:
         message = 'You searched for: {}'.format(request.GET['q'])
         message += "<br>"
-        try:
-            projects = Project.objects.filter(name__contains=request.GET['q'])
-            for p in projects:
-                message += "Project Name: {}<br>".format(p.name)
-                message += "<b>Admin(s):</b><br>"
-                for a in p.admins.all():
-                    message += "{}<br>".format(a)
-                message += "<b>Leader:</b><br>"
-                message += "{}<br>".format(p.leader)
-                message += "<b>Member(s):</b><br>"
-                for a in p.memberuser_set.all():
-                    message += "{}<br>".format(a)
-                message += "Log: {}<br>".format(p.activitylog.title)
-                message += "Log ID: {}<br>".format(p.activitylog.id)
-                message += "Log: {}<br>".format(p.actionlist.name)
-                message += "Log ID: {}<br>".format(p.actionlist.id)
-                message += "<hr><hr>"
 
-        except Exception as e:
-            message += '<b>No projects exist with this name.<br>'
-            message += e.__str__()
-            message += '</b>'
-            message += "<hr>"
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+        projects = Project.objects.filter(name__contains=request.GET['q'])
+        # for p in projects:
+        #     message += "Project Name: {}<br>".format(p.name)
+        #     message += "<b>Admin(s):</b><br>"
+        #     for a in p.admins.all():
+        #         message += "{}<br>".format(a)
+        #     message += "<b>Leader:</b><br>"
+        #     message += "{}<br>".format(p.leader)
+        #     message += "<b>Member(s):</b><br>"
+        #     for a in p.memberuser_set.all():
+        #         message += "{}<br>".format(a)
+        #     message += "Log: {}<br>".format(p.activitylog.title)
+        #     message += "Log ID: {}<br>".format(p.activitylog.id)
+        #     message += "Log: {}<br>".format(p.actionlist.name)
+        #     message += "Log ID: {}<br>".format(p.actionlist.id)
+        #     message += "<hr><hr>"
+        if request.GET['q'] is '':
+            errors.append('Enter a search term.')
+            return render(request,
+                          'users/search_form.html',
+                          {'errors': errors})
+
+        return render(request,
+                      'users/search_form.html',
+                      {
+                          'errors': errors,
+                          'projects': projects,
+                          'query': request.GET['q']
+                      }
+                      )
+
+    return render(request,
+                  'users/search_form.html',
+                  {'errors': errors})
